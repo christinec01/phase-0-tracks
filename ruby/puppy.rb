99e848138c439eb3cf2e2faf12bbc7dbb72305db
Christine Casebolt <christine.casebolt@gmail.com>
2016-06-29 09:31:23 -0700

    6.1 commit

---
+++ ruby/puppy.rb
@@ -0,0 +1,13 @@
+#species-------------------
+#puppy
+
+#characteristics----------
+#Fur color: yellow
+#ears: large and floppy
+#tail wag?: yes
+
+#behavior-----------------
+#happy
+#clumsy
+#potty trained
+#barks
