b5efb7fce3c5fb1290ee2dbc4ebe800f7bac9c62
Christine Casebolt <christine.casebolt@gmail.com>
2016-06-29 16:52:45 -0700

    release 1

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,35 @@
 class Puppy
 
+  def initialize
+    puts "Initializing new puppy instance..."
+  end
+
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
   end
 
+  def speak(integer)
+  	integer.times {puts "Woof!"}
+  end
+
+  def roll_over
+  	puts "Roll over."
+  end
+
+  def dog_years(human_years)
+  	p human_years * 7
+  end
+
+  def play_dead
+  	puts "I'm dead!"
+  end
 end
 
+# driver code
+puppy1 = Puppy.new
+puppy1.fetch("ball")
+puppy1.speak(3)
+puppy1.roll_over
+puppy1.dog_years(3)
+puppy1.play_dead
