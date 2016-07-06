ee33ac2649fcfe78a1cad5d7121691eea0c3bc26
Christine Casebolt <christine.casebolt@gmail.com>
2016-07-05 20:28:24 -0700

    release 1

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -22,44 +22,49 @@ end
 
 
 def reindeer_ranking
-  reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Pranceer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+  reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Pranceer", "Vixen", "Comet", "Cupid", "Donner"]
 end
 def age
   age = 0
+  puts "This santa is #{age} years old"
 end
  end
  example_ethnicities = ["caucasian", "european", "middle eastern", "pacific islander", "hispanic"]
  gender = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
  suit_color = ["red", "mahogany", "yellow", "blue", "beige"]
 santa = []
-# p santa << Santa.new("agender", "black")
-# p santa << Santa.new("female", "Latino")
-# p santa << Santa.new("bigender", "white")
-# p santa << Santa.new("male", "Japanese")
-# p santa << Santa.new("female", "prefer not to say")
-# p santa << Santa.new("gender fluid", "Mystical Creature(Unicorn)")
-# p santa << Santa.new("NA", "NA")
+p santa << Santa.new("agender", "black")
+p santa << Santa.new("female", "Latino")
+p santa << Santa.new("bigender", "white")
+p santa << Santa.new("male", "Japanese")
+p santa << Santa.new("female", "prefer not to say")
+p santa << Santa.new("gender fluid", "Mystical Creature(Unicorn)")
+p santa << Santa.new("NA", "NA")
 
 
- example_ethnicities.each do |example_ethnicity|
- santa << Santa.new(gender, example_ethnicity)
-   p "This Santa is #{example_ethnicity}!"
-  puts "=========================="
+ example_ethnicities.length.times do |i|
+ santa << Santa.new(gender[i], example_ethnicities[i])
 end
+# santa.length do |santa|
+# puts "There are now #{santa.length} Santas!"
+# puts "the santas can speak..."
+#
+#   santa.speak
+# end
+# example_ethnicities.each do |example_ethnicity|
+#   santa << Santa.new(gender, example_ethnicity)
+#   p "This Santa is #{example_ethnicity}!"
+#   puts "=========================="
+# end
 
 puts "There are now #{santa.length} Santas!"
 puts "the santas can speak..."
 santa.each do |santa|
   santa.speak
-end
-example_ethnicities.each do |example_ethnicity|
-  santa << Santa.new(gender, example_ethnicity)
-  p "This Santa is #{example_ethnicity}!"
-  puts "=========================="
+  santa.age
 end
 
-puts "There are now #{santa.length} Santas!"
-puts "the santas can speak..."
-santa.each do |santa|
-  santa.speak
+gender.length.times do |i|
+  santa<< Santa.new(gender[i], example_ethnicities[i])
+  puts "This santa is #{gender[i]}"
 end
