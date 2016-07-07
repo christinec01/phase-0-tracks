9dbf4a32ffdd16487b13a26c4e8e413d4c700a15
Christine Casebolt <christine.casebolt@gmail.com>
2016-07-07 09:07:55 -0700

    add user interface

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -3,7 +3,7 @@ class Magical_Unicorn # Magical unicorns class
     attr_accessor :horn_color
 
   def initialize(age, horn_color, gender) # attributes:
-    @age = age # age
+    @age = rand(0..300) # age
     @horn_color = horn_color # horn color
     @gender = gender # gender
   end
@@ -21,9 +21,23 @@ class Magical_Unicorn # Magical unicorns class
     puts "This unicorn's horn color was #{@horn_color}, now its #{@color_ator}!"#has the unicorn state its horn color, then change it to a new color
   end
 end
-unicorn_1 = Magical_Unicorn.new(12,"red", "female")
-p unicorn_1.age
-unicorn_1.horn_color = 'Blue'
-p unicorn_1.horn_color
-unicorn_1.speak("hi")
-unicorn_1.color_ator("yellow")
+# unicorn_1 = Magical_Unicorn.new(12,"red", "female")
+# p unicorn_1.age
+# unicorn_1.horn_color = 'Blue'
+# p unicorn_1.horn_color
+# unicorn_1.speak("hi")
+# unicorn_1.color_ator("yellow")
+unicorns = [] #creating an empty unicorns array to store these magical creatures
+puts "Hello! Would you like to create some unicorns?!(y/n)"
+  more_unicorns = "y"
+  unicorns_desired = gets.chomp
+      while unicorns_desired == more_unicorns
+      puts "Great! What color horn would you like it to have?"
+      @horn_color = gets.chomp
+      puts "what gender unicorn would you like? Female, Male, or Sparklehorse?"
+      @gender = gets.chomp
+      unicorns << Magical_Unicorn.new(@age,@horn_color, @gender)
+      puts "Would you like to make another unicorn?"
+      unicorns_desired = gets.chomp
+      end
+      p unicorns
