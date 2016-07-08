a97db63b9ff637b900bc601f66a582efedf7c11d
Christine Casebolt <christine.casebolt@gmail.com>
2016-07-07 17:13:33 -0700

    release one

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,10 @@
 module Shout
-  #code
+  def self.yell_angrily(words)
+    words + "!!!" + ":("
+  end
+  def self.yell_happily(words)
+    words + "!!!" + ":D :D :D"
+  end
+  p self.yell_angrily("nooo")
+  p self.yell_happily("OMG YES")
 end
