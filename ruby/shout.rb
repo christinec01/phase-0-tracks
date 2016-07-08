1ac1a15784f17e254f136d0250f99e5edd7423cc
Christine Casebolt <christine.casebolt@gmail.com>
2016-07-07 20:02:36 -0700

    correct branch, final commit

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,10 +1,31 @@
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + ":("
+#   end
+#   def self.yell_happily(words)
+#     words + "!!!" + ":D :D :D"
+#   end
+#   p self.yell_angrily("nooo")
+#   p self.yell_happily("OMG YES")
+# end
 module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + ":("
+  def happy
+    puts "I'm just SO happy!"
   end
-  def self.yell_happily(words)
-    words + "!!!" + ":D :D :D"
+  def angry
+    puts "I'm just SOO angry!"
   end
-  p self.yell_angrily("nooo")
-  p self.yell_happily("OMG YES")
 end
+class Person
+  include Shout
+end
+class Instructor
+  puts "I'm an instructor."
+  include Shout
+end
+person = Person.new
+person.happy
+person.angry
+
+instructor = Instructor.new
+instructor.happy
