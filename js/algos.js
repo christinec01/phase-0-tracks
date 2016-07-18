a4d3a9679a2ebec6882e860203574d5a917572fc
Christine Casebolt <christine.casebolt@gmail.com>
2016-07-18 00:25:30 -0700

    release 1

--- js/algos.js
+++ js/algos.js
@@ -1,5 +1,7 @@
+//Release 0
 //start with a variable that holds an array of strings of varying length
 var myArray = ["long phrase","longest phrase","longer phrase"]
+var testArray = ["hi", "bye", "hello"]
 //also creat a variable that will hold the current longest string
 function longestWord(myArray){
   var longestString = " "
@@ -13,5 +15,24 @@ function longestWord(myArray){
 
   return longestString
 }
-
+//Driver Code
 console.log(longestWord(myArray))
+console.log(longestWord(testArray))
+
+//Release 1
+//Write a function that takes two objects
+function keyValue(obj1, obj2){
+  //The fuction should check to see if the objects share a k:v pair
+  for (var key in obj1){
+    if(obj1[key] === obj2[key]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+obj1 = {name: "Steven", age: 54}
+obj3 = {name: "Steven", age: 54}
+
+obj2 = {name: "Tamir", age: 54}
+console.log(keyValue(obj1, obj2))
