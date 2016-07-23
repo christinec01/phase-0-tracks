eab996857d20666fd719838e7bfb611aa8bcc266
Christine Casebolt <christine.casebolt@gmail.com>
2016-07-23 15:16:26 -0700

    release 2

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -31,3 +31,93 @@ color_array.map! do |color|
 end
 puts "color array after .map!"
 puts color_array
+
+favorite_food = {
+  vegetable: "Chard",
+  fruit: "Strawberry",
+  dessert: "Pudding"
+  }
+
+puts "Hash before .each:"
+puts favorite_food
+
+favorite_food.each do |food_type, fav_food|
+  puts "hash in .each looop"
+  puts "#{food_type.upcase}" + " #{fav_food.upcase}"
+end
+
+#release two question oneletters_array = ["a", "e" , "i", "o", "u", "b"]
+puts "before .delete_if method"
+puts "#{letters_array}"
+letters_array.delete_if {|letter| letter == "b"}
+puts "after .delete_if method"
+puts "#{letters_array}"
+
+
+names = {
+  1 => "Alice",
+  2 => "Ann",
+  3 => "Christine",
+  4 => "Ilaria"
+  }
+
+names.delete_if do |number, name|
+  name.start_with? "A"
+end
+puts names
+
+#question two
+
+letters_array = ["a", "e" , "i", "o", "u", "b"]
+puts "before .keep_if method"
+puts "#{letters_array}"
+letters_array.keep_if {|letter| letter == "b"}
+puts "after .keep_if method"
+puts "#{letters_array}"
+
+
+names = {
+  1 => "Alice",
+  2 => "Ann",
+  3 => "Christine",
+  4 => "Ilaria"
+  }
+
+names.keep_if do |number, name|
+  name.start_with? "A"
+end
+puts names
+
+#question 3
+names = {
+  1 => "Alice",
+  2 => "Ann",
+  3 => "Christine",
+  4 => "Ilaria"
+  }
+new_names = names.reject do |number, name|
+   name.start_with? "A"
+end
+puts new_names
+
+
+
+letter_array = [1, 23, 4, 55, 9]
+new_array = letter_array.drop(3)
+puts new_array
+#question 4
+letter_array = [1, 23, 4, 55, 9]
+new_array = letter_array.drop_while do |item| item < 4
+end
+
+
+names = {
+  1 => "Alice",
+  2 => "Ann",
+  3 => "Christine",
+  4 => "Ilaria"
+  }
+new_names = names.reject do |number, name|
+ names.has_value?("Christine")
+end
+puts new_names
