c930447bcb31ed3ee1415d3b8b540b140b483c81
Christine Casebolt <christine.casebolt@gmail.com>
2016-07-23 18:45:50 -0700

    redo of nested data structures assignment @bontgoy

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,24 +1,19 @@
-highway = {
-  cars: {
-    color: ['blue','green', 'red', 'yellow'],
-    open_seats: {
-      total_seats: 4,
-      people:3
-    },
-    speeding: ["no", "slightly", "definitely"]
-  },
-  trucks: {
-    color: 'grey',
-    types: {
-      has_cargo: "yes",
-
-    },
-    speeding:["yes", "a little", "no"]
+  house = {
+    "Living_room"=>["lamp", "sofa"],
+    "Bathroom"=>["tub", "sink", "window"],
+    "Bedroom"=> ["bed", "dresser", "window"],
   }
 
-}
+def big_letters(hash,key,location)
+   hash[key][location].upcase
+end
+
+def furnish_house(hash, key, furniture_to_add)
+   hash[key].push(furniture_to_add)
+   puts "The #{key} now has a #{furniture_to_add}."
+end
 
 
-p highway[:cars][:speeding][0]
-p highway[:cars][:color][-1]
-p highway[:trucks][:speeding].push("definitely")
+
+puts big_letters(house,'Bedroom', 1)
+puts furnish_house(house, 'Living_room', "tv")
