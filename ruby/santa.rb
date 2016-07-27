4dd86bac40dab4ae9dfca1850f72501373ac892b
Christine Casebolt <christine.casebolt@gmail.com>
2016-07-26 23:21:56 -0700

    getter and setter code

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -11,6 +11,14 @@ class Santa
   def eat_milk_and_cookies(cookie_type)
     puts "That was a good #{cookie_type}."
   end
+#setter method
+  def celebrate_birthday
+    @age = age+1
+  end
+#getter method
+  def age
+    @age
+  end
 
 end
 
@@ -22,11 +30,15 @@ santa1.eat_milk_and_cookies('peanut butter cookie')
 #Driver code for release 1 to test gender, ethnicity, reindeer_ranking, and age attributes
 example_ethnicity = ["Hispanic", "Spanish", "Russian", "Jamaican", "Costa Rican", "Caucasian", "Indian"]
 example_gender = ["male", "female", "transgender", "gender fluid", "doesn't identify with a gender"]
-santas = []
-example_ethnicity.length.times do |i|
-  santas << Santa.new(example_gender[i],example_ethnicity[i])
-  santas
-end
- santas.each do |santa|
-  p santa
-end
+# santas = []
+# example_ethnicity.length.times do |i|
+#   santas << Santa.new(example_gender[i],example_ethnicity[i])
+#   santas
+# end
+#  santas.each do |santa|
+#   p santa
+# end
+
+puts "Santa number one was #{santa1.age} years old."
+puts "The he had a birthday and now he's #{santa1.celebrate_birthday}"
+puts "So, his new age is #{santa1.age}."
