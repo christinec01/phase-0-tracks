1f0829482b65342d576dba3eacd82ca5ed77e47c
Christine Casebolt <christine.casebolt@gmail.com>
2016-07-27 10:30:38 -0700

    complete santa challenge

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,6 +1,6 @@
 class Santa
-  attr_reader :age, :ethnicity
-  attr_accessor :gender,:reindeer
+  attr_reader :ethnicity
+  attr_accessor :gender, :reindeer_ranking, :age
 
   def initialize(gender, ethnicity)
     @gender = gender
@@ -14,14 +14,10 @@ class Santa
   def eat_milk_and_cookies(cookie_type)
     puts "That was a good #{cookie_type}."
   end
-#setter method
+
   def celebrate_birthday
     @age = age.to_i+1
   end
-
-  # def gender=(gender_assignment)
-  #   @gender = gender_assignment
-  # end
 
   def get_mad_at(reindeer_name)
     @reindeer_ranking.delete_if {|i| i ==reindeer_name}
@@ -29,23 +25,6 @@ class Santa
     @reindeer_ranking
     puts "Santa got mad at #{reindeer_name}."
   end
-
-#getter method
-  # def age
-  #   @age
-  # end
-  #
-  # def reindeer
-  #   @reindeer_ranking
-  # end
-  #
-  # def gender
-  #   @gender
-  # end
-  #
-  # def ethnicity
-  #   @ethnicity
-  # end
 end
 
 #Driver code for release 0
@@ -54,24 +33,33 @@ santa1.gender
 santa1.eat_milk_and_cookies('peanut butter cookie')
 
 #Driver code for release 1 to test gender, ethnicity, reindeer_ranking, and age attributes
+#Refactored code for release 4 to generate random santas with access to age, gender and ethnicity properties 
 example_ethnicity = ["Hispanic", "Spanish", "Russian", "Jamaican", "Costa Rican", "Caucasian", "Indian"]
 example_gender = ["male", "female", "transgender", "gender fluid", "doesn't identify with a gender"]
-# santas = []
-# example_ethnicity.length.times do |i|
-#   santas << Santa.new(example_gender[i],example_ethnicity[i])
-#   santas
-# end
-#  santas.each do |santa|
-#   p santa
-# end
+santas = []
+example_ethnicity.length.times do |i|
+  santas << Santa.new(example_gender.sample,example_ethnicity.sample)
+  santas
+end
+ santas.each do |santa|
+   santa.age = rand(0..140)
+    puts "Santa is #{santa.age} old"
+    puts "Santa's ethnicity is #{santa.ethnicity}"
+    puts "Santa's gender is #{santa.gender}"
+    puts
+end
+# santa2 = Santa.new(example_gender.sample, example_ethnicity.sample)
+# santa2.age = rand(0..140)
+# puts santa2.age
+
 puts "===========Testing ageing santa========="
 puts "Santa number one was #{santa1.age} years old."
 puts "The he had a birthday and now he's #{santa1.celebrate_birthday}"
 puts "So, his new age is #{santa1.age}."
 puts "=========Testing angery santa method on reindeer====="
-puts "These are Santa's reindeer in order of favorite to least favorite: #{santa1.reindeer}"
+puts "These are Santa's reindeer in order of favorite to least favorite: #{santa1.reindeer_ranking}"
 puts "As you can see, he has been bumped: #{santa1.get_mad_at("Dasher")}"
-puts "Now these are Santa's reindeer in order of favorite to lease favorite: #{santa1.reindeer}"
+puts "Now these are Santa's reindeer in order of favorite to lease favorite: #{santa1.reindeer_ranking}"
 
 puts "===========Testing gender changing santa========="
  puts "Santa was a #{santa1.gender}"
@@ -79,5 +67,3 @@ puts "===========Testing gender changing santa========="
  puts "Now Santa is a #{santa1.gender}"
 puts "===========Testing santa ethnicity========="
 puts "Santa's ethnicity is #{santa1.ethnicity}"
-# santa1.gender = "female"
-puts "santa's gender is #{santa1.gender}"
