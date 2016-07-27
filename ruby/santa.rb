4882d32b8766ff417a7844e57a885f2be67e70b0
Christine Casebolt <christine.casebolt@gmail.com>
2016-07-27 09:56:43 -0700

    refactor to use attr_reader and attr_accessor

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,4 +1,7 @@
 class Santa
+  attr_reader :age, :ethnicity
+  attr_accessor :gender,:reindeer
+
   def initialize(gender, ethnicity)
     @gender = gender
     @ethnicity = ethnicity
@@ -13,12 +16,12 @@ class Santa
   end
 #setter method
   def celebrate_birthday
-    @age = age+1
+    @age = age.to_i+1
   end
 
-  def set_gender(gender_assignment)
-    @gender = gender_assignment
-  end
+  # def gender=(gender_assignment)
+  #   @gender = gender_assignment
+  # end
 
   def get_mad_at(reindeer_name)
     @reindeer_ranking.delete_if {|i| i ==reindeer_name}
@@ -28,39 +31,39 @@ class Santa
   end
 
 #getter method
-  def age
-    @age
-  end
-
-  def reindeer
-    @reindeer_ranking
-  end
-
-  def gender
-    @gender
-  end
-
-  def ethnicity
-    @ethnicity
-  end
+  # def age
+  #   @age
+  # end
+  #
+  # def reindeer
+  #   @reindeer_ranking
+  # end
+  #
+  # def gender
+  #   @gender
+  # end
+  #
+  # def ethnicity
+  #   @ethnicity
+  # end
 end
 
 #Driver code for release 0
-santa1 = Santa.new("agender","aethnicity")
-santa1.speak
+santa1 = Santa.new("agender", "aethnicity")
+santa1.gender
 santa1.eat_milk_and_cookies('peanut butter cookie')
 
 #Driver code for release 1 to test gender, ethnicity, reindeer_ranking, and age attributes
 example_ethnicity = ["Hispanic", "Spanish", "Russian", "Jamaican", "Costa Rican", "Caucasian", "Indian"]
 example_gender = ["male", "female", "transgender", "gender fluid", "doesn't identify with a gender"]
-santas = []
-example_ethnicity.length.times do |i|
-  santas << Santa.new(example_gender[i],example_ethnicity[i])
-  santas
-end
- santas.each do |santa|
-  p santa
-end
+# santas = []
+# example_ethnicity.length.times do |i|
+#   santas << Santa.new(example_gender[i],example_ethnicity[i])
+#   santas
+# end
+#  santas.each do |santa|
+#   p santa
+# end
 puts "===========Testing ageing santa========="
 puts "Santa number one was #{santa1.age} years old."
 puts "The he had a birthday and now he's #{santa1.celebrate_birthday}"
@@ -71,7 +74,9 @@ puts "As you can see, he has been bumped: #{santa1.get_mad_at("Dasher")}"
 puts "Now these are Santa's reindeer in order of favorite to lease favorite: #{santa1.reindeer}"
 
 puts "===========Testing gender changing santa========="
- puts "#{santa1.set_gender("male")}"
+ puts "Santa was a #{santa1.gender}"
+ santa1.gender = "male"
+ puts "Now Santa is a #{santa1.gender}"
 puts "===========Testing santa ethnicity========="
 puts "Santa's ethnicity is #{santa1.ethnicity}"
 # santa1.gender = "female"
