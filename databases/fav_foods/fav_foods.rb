5d8be312c6a59a3284106c6d36efa38555196cfa
Christine Casebolt <christine.casebolt@gmail.com>
2016-08-13 15:57:44 -0700

    work on using faker

--- databases/fav_foods/fav_foods.rb
+++ databases/fav_foods/fav_foods.rb
@@ -34,10 +34,19 @@ db.execute(create_restaurants_table_cmd)
 # VALUES ('Papas', '1234 Italiano Way', 1, 'June 3, 2014')")
 
 #explore ORM
-restaurants = db.execute("select * from restaurants")
-puts restaurants.class
-p restaurants
 
 #use iteration to create many instances of restaurants
+def create_ratings(db,name,address,rating, date_visited)
+date_visited = Faker::Date.backward(14).to_s
+db.execute("Insert into restaurants (name, address, rating, date_visited) values (?, ?, ?, ?)", [name, address, rating, date_visited])
+end
 
+10.times do
+  create_ratings(db,Faker::Company.name, Faker::Address.street_address, 3, date_visited)
+end
+
+restaurants = db.execute("select * from restaurants")
+restaurants.each do |restaurant|
+  puts "You rated #{restaurant['name']} #{restaurant['rating']} star(s) out of 5 last time you were there which was on #{restaurant['date_visited']}."
+end
 # add user interface that allows additional restaurant reviews to be added
