2af413620b620200dfae127efc420d2de097c690
Christine Casebolt <christine.casebolt@gmail.com>
2016-08-16 09:40:17 -0700

    add event handler

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -7,4 +7,12 @@ function addDottedBorder() {
      ourDiv.style.border = "10px dotted black";
 }
 
+function changeHeaderColor() {
+  var ourHeaderColor = document.getElementById("main header");
+  ourHeaderColor.style.color = "red";
+}
+
+document.getElementById("main header").addEventListener("click",changeHeaderColor);
+
+// changeHeaderColor();
 addDottedBorder();
