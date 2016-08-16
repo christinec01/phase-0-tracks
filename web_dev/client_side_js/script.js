fc9bedd5c93ea3f6fec954142653a7be95ba037e
Christine Casebolt <christine.casebolt@gmail.com>
2016-08-15 19:08:56 -0700

    add js file functionality to DOM

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,5 +1,10 @@
-// function addDottedBorder() {
-  var ourDiv = document.getElementsByTagName("div");
-  ourDiv.style.border = "10px dotted black";
-// }
-// addDottedBorder();
+
+console.log ("The script is running");
+
+
+function addDottedBorder() {
+    var ourDiv=document.getElementById("main div");
+     ourDiv.style.border = "10px dotted black";
+}
+
+addDottedBorder();
